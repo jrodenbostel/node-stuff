96350bbe562a3c46bf3a7e50f5910907f3854736
Justin Rodenbostel <justin@rodenbostel.com>
2014-08-21 17:11:33 -0500

    latest changes

--- node-in-action/chatroom/lib/chat_server.js
+++ node-in-action/chatroom/lib/chat_server.js
@@ -1,9 +1,9 @@
 var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
-var nickNames = {};
-var namesUsed = [];
-var currentRoom = {};
+var nickNames = {}; //maps socket ids to names
+var namesUsed = []; //keeps a current list of names
+var currentRoom = {}; //holds a map where the key is the socket id and the value is the current room.
 
 function listen(server) {
 	io = socketio.listen(server);
@@ -20,6 +20,96 @@ function listen(server) {
 		});
 		
 		handleClientDisconnection(socket, nickNames, namesUsed);
+	});
+}
+
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+	var name = 'Guest' + guestNumber;
+	nickNames[socket.id] = name;
+	socket.emit('nameResult', { success: true, name: name });
+	namesUsed.push(name);
+	return guestNumber + 1;
+}
+
+function joinRoom(socket, room) {
+	socket.join(room); //becomes a listener to the current channel (room in this case is lobby, socket is the user's connection)
+	currentRoom[socket.id] = room;
+	socket.emit('joinResult', {room: room}); //hits the user's socket with a 'joinResult' event containing the room info
+	socket.broadcast.to(room).emit('message', { //send the message event to everyone on the 'Lobby' channel
+		text: nickNames[socket.id] + ' has joined ' + room + '.';
+	});
+	
+	var usersInRoom = io.sockets.clients(room); //returns the list of socket ids in the current channel
+	if(usersInRoom.length > 1) {
+		var usersInRoomSummary = 'Users currently in ' + room + ': ';
+		for (var index in usersInRoom) {
+			var userSocketId = usersInRoom[index].id;
+			if (userSocketId != socket.id) { //filters out the current user's socket, displays everyone elses.
+				if(index > 0) {
+					usersInRoomSummary += ', ';
+				}
+				usersInRoomSummary += nickNames[userSocketId];
+			}
+		}
+		usersInRoomSummary += '.';
+		socket.emit('message', {text: usersInRoomSummary}); //sends a message back to the client socket.
+	}
+}
+
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+	socket.on('nameAttempt', function(name) { //listen for nameAttempt event on current socket
+		if(name.indexOf('Guest') == 0) {
+			socket.emit('nameResult', {
+				success: false,
+				message: 'Names cannot begin with "Guest".'
+			});
+		} else {
+			if(namesUsed.indexOf(name) == -1) { //name is not found in the current set of names
+				var previousName = nickNames[socket.id];
+				var previousNameIndex = namesUsed.indexOf(previousName); //find the old name in the set of names
+				namesUsed.push(name); //add the new name
+				nickNames[socket.id] = name; //assign the new name
+				delete namesUsed[previousNameIndex]; // remove the old name
+				socket.emit('nameResult', {
+					success: true,
+					name: name
+				});
+				socket.broadcast.to(currentRoom[socket.id]).emit('message', { //from the current connection, broadcasts to the room the current connection is in.
+					text: previousName + ' is now known as ' + name + '.';
+				});
+			} else {
+				socket.emit('nameResult', {
+					success: false,
+					message: 'That name is already in use.'
+				});
+			}
+		}
+	});
+}
+
+/**
+* relays message from passed-in socket to everyone in the room the current socket is a member of.
+**/
+function handleMessageBroadcasting(socket) {
+	socket.on('message', function(message) {
+		socket.broadcast.to(message.room).emit('message', {
+			text: nickNames[socket.id] + ': ' + message.text
+		});
+	});
+}
+
+function handleRoomJoining(socket) {
+	socket.on('join', function(room) {
+		socket.leave(currentRoom[socket.id]);
+		joinRoom(socket, room.newRoom); //new room must be a parameter on the join event that is required
+	});
+}
+
+function handleClientDisconnection(socket) {
+	socket.on('disconnect', function() {
+		var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+		delete namesUsed[nameIndex];
+		delete nickNames[socket.id];
 	});
 }
 
